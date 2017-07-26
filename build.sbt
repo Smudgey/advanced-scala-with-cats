08e24d49a47f9a1ec82290bcd9db203c148e4ae0
Luke Smith <lukesmudgesmith@gmail.com>
2017-07-26 11:33:36 +0100

    1.2 Meet Cats

--- build.sbt
+++ build.sbt
@@ -3,4 +3,5 @@ name := "advanced-scala-with-cats"
 version := "1.0"
 
 scalaVersion := "2.12.1"
-        
+
+libraryDependencies += "org.typelevel" %% "cats" % "0.9.0"
--- src/main/scala/Ch1_Introduction.scala
+++ src/main/scala/Ch1_Introduction.scala
@@ -1,55 +1,62 @@
 
 /**
+  * 1.1 - Anatomy of a Type Class
+  *
   * The Scala implementation of a type class has three parts:
   * • the type class itself, a generic trait;
   * • instances for each type we care about; and
   * • one or more generic interface methods.
   */
 
-// The TYPE CLASS itself
-trait Printable[A] {
-  def format(value: A): String
-}
+object AnatomyOfATypeClass extends App {
 
-// INSTANCES of Printable
-object PrintableInstances {
-  implicit val stringPrintable = new Printable[String] {
-    def format(value: String): String = value
+  // The TYPE CLASS itself
+  trait Printable[A] {
+    def format(value: A): String
   }
-  implicit val intPrintable = new Printable[Int] {
-    def format(value: Int): String = value.toString
-  }
-  implicit val catPrintable = new Printable[Cat] {
-    def format(cat: Cat): String = {
-      val name = Printable.format(cat.name)
-      val age = Printable.format(cat.age)
-      val colour = Printable.format(cat.colour)
-      s"$name is a $age year-old $colour cat."
+
+  // INSTANCES of Printable
+  object PrintableInstances {
+    implicit val stringPrintable = new Printable[String] {
+      def format(value: String): String = value
+    }
+    implicit val intPrintable = new Printable[Int] {
+      def format(value: Int): String = value.toString
+    }
+    implicit val catPrintable = new Printable[Cat] {
+      def format(cat: Cat): String = {
+        val name = Printable.format(cat.name)
+        val age = Printable.format(cat.age)
+        val colour = Printable.format(cat.colour)
+        s"$name is a $age year-old $colour cat."
+      }
     }
   }
-}
 
-// The INTERFACE object - this is the object you use
-object Printable {
-  def format[A](value: A)(implicit printable: Printable[A]): String = printable.format(value)
+  /**
+    * Interface methods can be defined in interface objects or interface syntax.
+    * Implicit classes are the most common way of implementing syntax.
+    */
 
-  def print[A](value: A)(implicit printable: Printable[A]): Unit = println(format(value))
-}
+  // The INTERFACE object - this is the object you use
+  object Printable {
+    def format[A](value: A)(implicit printable: Printable[A]): String = printable.format(value)
 
-// INTERFACE object
-object PrintableSyntax {
-
-  implicit class PrintOps[A](value: A) {
-    def format(implicit printable: Printable[A]): String = printable.format(value)
-
-    def print(implicit printable: Printable[A]): Unit = println(format)
+    def print[A](value: A)(implicit printable: Printable[A]): Unit = println(format(value))
   }
 
-}
+  // INTERFACE syntax
+  object PrintableSyntax {
 
-final case class Cat(name: String, age: Int, colour: String)
+    implicit class PrintOps[A](value: A) {
+      def format(implicit printable: Printable[A]): String = printable.format(value)
 
-object MainApp extends App {
+      def print(implicit printable: Printable[A]): Unit = println(format)
+    }
+
+  }
+
+  final case class Cat(name: String, age: Int, colour: String)
 
   import PrintableInstances._
   import PrintableSyntax._
@@ -59,4 +66,34 @@ object MainApp extends App {
   Printable.print(cat)
 
   Cat("Smudge", 7, "Black").print
+}
+
+/**
+  * 1.2 - Meet Cats
+  *
+  * Cats type classes are defined in the cats package. For example, the
+  * Show type class is defined as cats.Show .
+  *
+  * Default instances are defined in the cats.instances package. Imports
+  * are organized by parameter type (as opposed to by type class).
+  *
+  * Interface syntax is defined in the cats.syntax package. There are separate
+  * syntax imports for each type class. For example, the syntax for
+  * Show is defined in cats.syntax.show .
+  */
+
+object MeetCats extends App {
+
+  import cats.Show
+  import cats.instances.int._
+  import cats.instances.string._
+  import cats.syntax.show._
+
+  final case class Cat(name: String, age: Int, colour: String)
+
+  val cat = Cat("Smudge", 7, "Black")
+
+  implicit val catShow: Show[Cat] = Show.show(cat => s"${cat.name.show} is a ${cat.age.show} year-old ${cat.colour.show} cat.")
+
+  println(cat.show)
 }
